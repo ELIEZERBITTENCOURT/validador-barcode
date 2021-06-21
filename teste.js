7042b441cf90e44c0ccdc132b2825c8ca81f135d
ELIEZER BITTENCOURT <66735493+ELIEZERBITTENCOURT@users.noreply.github.com>
2021-06-21 10:04:38 -0300

    Add mais um teste

--- teste.js
+++ teste.js
@@ -2,10 +2,12 @@ const boletofunc = require('./src/index') // Importando o módulo e atribuindo n
 
 // Número do boleto para teste
 // O número pode conter pontos e espaços pois serão tratados
-let boletocodigo = "74891 12024 03943.907380 23105.411054 2 82820000051180"
+//let boletocodigo = "74891 12024 03943.907380 23105.411054 2 82820000051180"
 //let boletocodigo = "123482938102381039810293810938093819023810982309182301238109238109328091"
 //let boletocodigo = "10499898100000214032006561000100040099726390"
 //let boletocodigo = "23797000000000000004150090019801673500021140"
+//let boletocodigo = "10492006506100010004200997263900989810000021403"
+let boletocodigo = "23797000000000000004150090019801673500021140"
 
 // Retorna true ou false se o codigo do boleto é válido ou não
 let validacao = boletofunc.validarBoleto(boletocodigo)
